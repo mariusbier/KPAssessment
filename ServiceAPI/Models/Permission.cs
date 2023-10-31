aa478e97dee1087ee92bcfc823f10424467c351d
Marius <Marius@typedev.co.za>
2023-10-31 20:43:05 +0200

    Removed wrong projects

--- ServiceAPI/Models/Permission.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ServiceAPI.Models
-{
-    public class Permission
-    {
-        public Guid PermissionId { get; set; } = Guid.NewGuid();
-        public string Name { get; set; }
-    }
-}
